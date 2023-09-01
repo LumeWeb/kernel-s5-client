b907b6f276a35a25519f5c334e4356e80e83b795
Derrick Hammer <derrick@derrickhammer.com>
2023-09-01 09:40:08 -0400

    feat: initial version

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { factory, NetworkClient } from "@lumeweb/libkernel/module";
+import type { SignedRegistryEntry } from "@lumeweb/libs5";
+
+export const MODULE =
+  "zduPehBQc2edPXDBsxo9CM6pTnS6bpDx1MBS92GH5wLAqMnErsVKhnobD1";
+
+export interface RegistryEntry {
+  key: Uint8Array;
+  data: Uint8Array;
+  revision: number;
+}
+
+export class S5Client extends NetworkClient {
+  public async getRegistryEntry(pubkey: Uint8Array | Buffer | string) {
+    return this.callModuleReturn("getRegistryEntry", { pubkey });
+  }
+
+  public async setRegistryEntry({
+    key,
+    data,
+    revision,
+  }: RegistryEntry): Promise<SignedRegistryEntry> {
+    return this.callModuleReturn("setRegistryEntry", { key, data, revision });
+  }
+
+  public registrySubscription(
+    pubkey: Uint8Array,
+    cb: (sre: SignedRegistryEntry) => void,
+  ): () => void {
+    let done = false;
+
+    const [end, ret] = this.connectModule(
+      "registrySubscription",
+      { pubkey },
+      cb,
+    );
+
+    ret.then((ret) => {
+      this.handleError(ret);
+    });
+
+    return () => {
+      if (done) {
+        return;
+      }
+      done = true;
+      end();
+    };
+  }
+}
+
+export const createClient = factory<S5Client>(S5Client, MODULE);
